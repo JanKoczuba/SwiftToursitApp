9ec0a817ba7d2d97d82b86cbd5d9eea07cfc362c
Jan Koczuba <schizobi@gmail.com>
2025-02-17 13:23:09 +0100

    added basic structure, assets, mocked service

---
+++ TouristApp/Model/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  TouristApp
+//
+//  Created by Jan Koczuba on 17/02/2025.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
