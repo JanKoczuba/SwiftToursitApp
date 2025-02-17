398bd105e2ce72d17f1d2ab3baa04fec564fb708
Jan Koczuba <schizobi@gmail.com>
2025-02-17 09:28:47 +0100

    Initial Commit

---
+++ TouristApp/TouristAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouristAppApp.swift
+//  TouristApp
+//
+//  Created by Jan Koczuba on 17/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct TouristAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TouristAppTests/TouristAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TouristAppTests.swift
+//  TouristAppTests
+//
+//  Created by Jan Koczuba on 17/02/2025.
+//
+
+import Testing
+@testable import TouristApp
+
+struct TouristAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
