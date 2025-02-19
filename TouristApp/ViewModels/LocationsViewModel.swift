18b1b3aa0cd05109fccaad3b6a9d879df0e58b83
Jan Koczuba <schizobi@gmail.com>
2025-02-19 11:47:23 +0100

    added locations preview card

--- TouristApp/ViewModels/LocationsViewModel.swift
+++ TouristApp/ViewModels/LocationsViewModel.swift
@@ -54,11 +54,33 @@ class LocationsViewModel: ObservableObject {
             showLocationsList = !showLocationsList
         }
     }
-    
-    func showNextLocation(location: Location){
-        withAnimation(.easeInOut){
+
+    func showNextLocation(location: Location) {
+        withAnimation(.easeInOut) {
             mapLocation = location
             showLocationsList = false
         }
+    }
+
+    func nextButtonPressed() {
+
+        guard
+            let currentIndex = locations.firstIndex(where: { $0 == mapLocation }
+            )
+        else {
+            return
+        }
+
+        let nextIndex = currentIndex + 1
+        guard locations.indices.contains(nextIndex) else {
+            guard let firstLocation = locations.first else { return }
+            showNextLocation(location: firstLocation)
+            return
+        }
+
+        let nextLocation = locations[nextIndex]
+
+        showNextLocation(location: nextLocation)
+
     }
 }
